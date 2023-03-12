5c7845c0981c065618f5b5e6b4eea7bbe83bfc5a
Daniyal Mahmood <daniyalmahmood123@gmail.com>
2023-03-12 16:33:53 +0530

    Add day5 code 	new file:   day5/bin/Test.class 	new file:   day5/bin/com/veera/Logic.class 	new file:   day5/lib/day5.jar 	new file:   day5/src/Test.java 	new file:   day5/src/com/veera/Logic.java

---
+++ day5/src/Test.java
@@ -0,0 +1,8 @@
+import com.veera.*;
+public class Test{
+    public static void main(String[] args) {
+        Logic r = new Logic();
+        System.out.println(r.area(5, 6));
+        System.out.println(r.simple_interest(5, 6, 7.5));
+    }
+}
---
+++ day5/src/com/veera/Logic.java
@@ -0,0 +1,9 @@
+package com.veera;
+public class Logic {
+    public int area(int l, int b) {
+        return l*b;
+    }
+    public double simple_interest(int p, double r, double t) {
+        return p*r*t/100;
+    }
+}
